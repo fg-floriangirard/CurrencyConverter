71e9e2d68f6f529b477b1f02698573617b00aff4
fg-floriangirard <fg.girardflorian@gmail.com>
2023-07-27 17:29:48 +0200

    Dynamic currencies using data currencies data file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,15 @@ import Header from '../Header/Header';
 import Currencies from '../Currencies/Currencies';
 import Amount from '../Amount/Amount';
 
+import currencies from '../../data/currencies';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Currencies />
+      <Currencies currenciesList={currencies} />
       <Amount />
     </div>
   );
--- src/components/Currencies/Currencies.jsx
+++ src/components/Currencies/Currencies.jsx
@@ -1,14 +1,30 @@
+// Import the PropTypes library for type-checking props
+import PropTypes from 'prop-types';
+
 import './Currencies.scss';
 
-const Currencies = () => (
+// Define the functional component 'Currencies' that takes 'currenciesList' as a prop
+const Currencies = ({ currenciesList }) => (
   <div className="currencies">
     <h3 className="currencies-title">Currencies</h3>
     <ul>
-      <li className="currency">USD</li>
-      <li className="currency">EUR</li>
-      <li className="currency">KRW</li>
+      {/* Iterate over each item in 'currenciesList' and create an 'li' element */}
+      {currenciesList.map((item) => (
+        <li className="currency" key={item.name}>
+          {item.name}
+        </li>
+      ))}
     </ul>
   </div>
 );
+
+// Define the prop types for 'currenciesList' prop
+Currencies.propTypes = {
+  currenciesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default Currencies;
